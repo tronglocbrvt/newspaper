ef811629ea883323ff160edf7eacb50a60413939
ndhp2000 <ndhp2000@gmail.com>
2021-06-24 17:44:08 +0700

    add link to tags and categories in article

--- controllers/article.router.js
+++ controllers/article.router.js
@@ -7,16 +7,29 @@ const { createConnection } = require('mysql2/promise');
 const article_model = require('../models/article.model')
 const router = express.Router();
 
+function formatTime(s)
+{
+    var date = new Date(s);
+    return date.toLocaleString("en-US");
+}
+
 /**
  * API get Article by IDs:
  */
 router.get('/:id', async function(req, res)
 {
     const article_id = req.params.id || 0;
-    const article = await article_model.find_by_id(article_id);
-    console.log(article[0]);
+    const db_data_article = await article_model.find_by_id(article_id);
+    const db_data_tags = await article_model.find_tags_by_id(article_id);
+
+    var article = db_data_article[0][0];
+    article.time_published = formatTime(article.time_published);
+    var tags = db_data_tags[0];
+    console.log(tags);
+
     res.render('vwArticle/view', {
-        article: article[0][0]
+        article: article,
+        tags : tags
     });
 });
 
--- models/article.model.js
+++ models/article.model.js
@@ -5,11 +5,20 @@ module.exports=
     find_by_id(id){
         prams = {id:id};
         const sql_query = `
-
-        SELECT pa.time_published, a.article_title, a.article_abstract, c.category_id as subcate_id, c.category_name as sub_category, m_c.category_id as maincate_id, m_c.category_name as main_category
+        SELECT pa.time_published, a.article_title, a.article_abstract, c.category_id as subcate_id, c.category_name as sub_category, m_c.category_id as maincate_id, m_c.category_name as main_category, CONCAT('/categories/',m_c.category_id) as maincate_ref, CONCAT('/categories/',c.category_id) as subcate_ref
         from published_articles as pa, articles as a, categories as c, categories as m_c
-        where pa.published_article_id=? and pa.article_id = a.article_id and a.category_id = c.category_id and c.parent_category_id = m_c.category_id`
+        where pa.published_article_id=? and pa.article_id = a.article_id and a.category_id = c.category_id and c.parent_category_id = m_c.category_id;`
         return db.raw(sql_query,id);
     },
+
+    find_tags_by_id(id)
+    {
+        const sql_query = `
+        SELECT T.tag_id,T.tag_name, CONCAT('/tags/',T.tag_id) as tag_link
+        from tags as T, tag_links as TL, articles as A
+        where A.article_id=? and  T.tag_id = TL.tag_id and TL.article_id =A.article_id;
+        `
+        return db.raw(sql_query,id);
+    }
 }
 
