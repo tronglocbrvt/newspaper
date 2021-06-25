b41e08c5066e4754f6195392b2e690440da97c8d
linh0303052 <ltt.lop9a1.lhlinh@gmail.com>
2021-06-25 21:49:32 +0700

    hard home

--- static/js/home.js
+++ static/js/home.js
@@ -14,15 +14,20 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
+
+$(document).ready(function(){
+  // setInterval(function(){ plusSlides(1) }, 5000);
+
+  $('#most-article').click(function(){
+    $(this).addClass('active');
+    $('#latest-article').removeClass('active');
+
+  });
+});
