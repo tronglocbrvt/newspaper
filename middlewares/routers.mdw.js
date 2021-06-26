7dca1a0d418ca9b863cad4ffe670aa6fc72b1561
ndhp2000 <ndhp2000@gmail.com>
2021-06-26 22:04:43 +0700

    pull from orign

--- middlewares/routers.mdw.js
+++ middlewares/routers.mdw.js
@@ -3,14 +3,11 @@ module.exports=function(app){
         res.render('home')
     });
     app.use('/categories', require('../controllers/category.router'));
-<<<<<<< HEAD
     app.use('/articles',require('../controllers/article.router'))
-=======
     app.use('/categories/:category_id', function(req, res, next) {
         req.cat_id = req.params.category_id;
         next();
     }, require('../controllers/subcategory.router'));
     app.use('/article',require('../controllers/article.router'))
     //app.use('/tags', require('../controllers/tag.router'));
->>>>>>> trongloc
 };
