a3a142f7eb7a29f24baa53f2b8c9192ee871f902
linh0303052 <ltt.lop9a1.lhlinh@gmail.com>
2021-06-26 22:48:34 +0700

    db

--- utils/db.js
+++ utils/db.js
@@ -3,8 +3,8 @@ const knex = require('knex')({
     connection: {
       host : '127.0.0.1',
       user : 'root',
-      password : 'LinhLocPhuc11122000@gmail.com',
-      database : 'LLP_NEWSPAPER',
+      password : '',
+      database : 'llpwebdatabase',
       port: 3306
     }
   });
