81d2c75be33e333b74b2035c0bfaa71431178c29
ndhp2000 <ndhp2000@gmail.com>
2021-08-04 00:33:50 +0700

    deploy

---
+++ env.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+module.exports = {
+    email_username: process.env.EMAIL_USERNAME,
+    email_password: process.env.EMAIL_PASSWORD,
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+}
