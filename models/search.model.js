75e22fdd39eed800f938f0aefac9a427063eb034
tronglocfs <ttloc@fossil.com>
2021-08-12 12:20:00 +0700

    sort relevance in search

--- models/search.model.js
+++ models/search.model.js
@@ -9,14 +9,15 @@ module.exports={
      */
     get_articles_from_search(keyword, type_search, offset, premium, time){
         //const params = { kw: keyword, type: type_search };
-        const sql = `select distinct a.*, p.published_article_id, p.time_published, c.parent_category_id, c.category_name, p.views_numbers
+        const sql = `select distinct a.*, p.published_article_id, p.time_published, c.parent_category_id, c.category_name, p.views_numbers,
+        MATCH(a.${type_search}) AGAINST('${keyword}' IN NATURAL LANGUAGE MODE) as relevance
         from articles as a, published_articles as p, categories as c
         where p.article_id = a.article_id
             and c.category_id = a.category_id
             and MATCH(a.${type_search}) AGAINST('${keyword}' IN NATURAL LANGUAGE MODE)
             and a.is_premium <= ${premium}
             and unix_timestamp(p.time_published) <= ${time}
-        order by a.is_premium DESC, a.article_id ASC
+        order by a.is_premium DESC, relevance DESC, a.article_id ASC
         limit 10 offset ${offset}`;
 
         return db.raw(sql);
